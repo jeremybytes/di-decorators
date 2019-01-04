07b0583b8f0a94358149e8f9fe53a11ed5a8c118
Jeremy Clark <jeremy@jeremybytes.com>
2019-01-04 11:15:54 -0800

    RetryReader test

---
+++ PersonReader.Decorator.Tests/BrokenReader.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonReader.Decorator.Tests
+{
+    public class BrokenReader : IPersonReader
+    {
+        private int brokenCount;
+        private int retryCount;
+
+        List<Person> testPeople = new List<Person>()
+            {
+                new Person() {Id = 1,
+                    GivenName = "John", FamilyName = "Smith",
+                    Rating = 7, StartDate = new DateTime(2000, 10, 1)},
+                new Person() {Id = 2,
+                    GivenName = "Mary", FamilyName = "Thomas",
+                    Rating = 9, StartDate = new DateTime(1971, 7, 23)},
+            };
+
+        public BrokenReader(int timesBroken = 0)
+        {
+            brokenCount = timesBroken;
+            retryCount = 0;
+        }
+
+        public async Task<IEnumerable<Person>> GetPeople()
+        {
+            if (retryCount < brokenCount)
+            {
+                retryCount++;
+                throw new Exception("Cannot retrieve data");
+            }
+
+            await Task.Delay(1);
+            return testPeople;
+        }
+
+        public Task<Person> GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PersonReader.Decorator.Tests/RetryReaderTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using PersonReader.Decorators;
+using System;
+using System.Threading.Tasks;
+
+namespace PersonReader.Decorator.Tests
+{
+    public class RetryReaderTests
+    {
+        [Test]
+        public async Task GetPeople_Broken0_ReturnsPeople()
+        {
+            var fakeReader = new BrokenReader(0);
+            var reader = new RetryReader(fakeReader);
+
+            var result = await reader.GetPeople();
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [Category("Slow")]
+        public async Task GetPeople_Broken1_ReturnsPeople()
+        {
+            var fakeReader = new BrokenReader(1);
+            var reader = new RetryReader(fakeReader);
+
+            var result = await reader.GetPeople();
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [Category("Slow")]
+        public async Task GetPeople_Broken3_ThrowsException()
+        {
+            var fakeReader = new BrokenReader(3);
+            var reader = new RetryReader(fakeReader);
+
+            try
+            {
+                var result = await reader.GetPeople();
+                Assert.Fail("Exception was not thrown");
+            }
+            catch (Exception)
+            {
+                Assert.Pass("Exception thrown");
+            }
+        }
+    }
+}
