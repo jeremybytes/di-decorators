8a18c6fa090685367cbaf7364cf0d538f646a31f
Jeremy Clark <jeremy@jeremybytes.com>
2019-04-08 16:22:03 -0700

    Add .NET Core 3.0 (Preview) WPF Project

---
+++ PeopleViewer.Core/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Logging;
+using PeopleViewer.Presentation;
+using PersonReader.CSV;
+using PersonReader.Decorators;
+using PersonReader.Service;
+using PersonReader.SQL;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Application.Current.DispatcherUnhandledException += UnhandledException;
+            ComposeObjects();
+            Application.Current.MainWindow.Show();
+        }
+
+        private void UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unable to process request. Please try again later.");
+            e.Handled = true;
+        }
+
+        private static void ComposeObjects()
+        {
+            var reader = new SQLReader();
+            var delay = new TimeSpan(0, 0, 3);
+            var retryReader = new RetryReader(reader, delay);
+            var logger = new FileLogger();
+            var loggingReader = new ExceptionLoggingReader(retryReader, logger);
+            var duration = new TimeSpan(0, 0, 10);
+            var cachingReader = new CachingReader(loggingReader, duration);
+            var viewModel = new PeopleReaderViewModel(cachingReader);
+            Application.Current.MainWindow = new MainWindow(viewModel);
+        }
+
+        private static void AlternateComposeObjects()
+        {
+            Application.Current.MainWindow =
+                new MainWindow(
+                    new PeopleReaderViewModel(
+                        new CachingReader(
+                            new ExceptionLoggingReader(
+                                new RetryReader(
+                                    new SQLReader(),
+                                    new TimeSpan(0, 0, 3)),
+                                new FileLogger()),
+                            new TimeSpan(0, 0, 10))));
+        }
+    }
+}
---
+++ PeopleViewer.Core/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PeopleViewer.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        IPeopleViewModel ViewModel { get; set; }
+
+        public MainWindow(IPeopleViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = ViewModel;
+        }
+
+        private async void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.RefreshPeople();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ClearPeople();
+        }
+    }
+}
